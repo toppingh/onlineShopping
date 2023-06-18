05b7b268b63c00f6f7ae33c20ad399ddf572026a
topping <reyna11@naver.com>
2023-06-18 16:29:02 +0900

    DB setting and shop model create

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'shop',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'examShop',
+        'USER': 'topping',
+        'PASSWORD': 'ekdms1122',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+# 카테고리 모델
+class Category(models.Model):
+    name = models.CharField(max_length=30, db_index=True)
+    meta_description = models.TextField(blank=True)
+
+    slug = models.SlugField(max_length=200, db_index=True, allow_unicode=True)
+
+    class Meta:
+        ordering=['name']
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_in_category', args=[self.slug])
+
+# 상품
