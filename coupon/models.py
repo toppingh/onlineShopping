11837f49e25e4a5d16f144aac9c42b7cc64e637e
topping <reyna11@naver.com>
2023-06-20 13:55:44 +0900

    edited coupon model

--- coupon/models.py
+++ coupon/models.py
@@ -7,7 +7,7 @@ class Coupon(models.Model):
     code = models.CharField(max_length=50, unique=True)
     use_from = models.DateTimeField()
     use_to = models.DateTimeField()
-    amount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(1000)])
+    amount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100000)])
     active = models.BooleanField()
 
     def __str__(self):
